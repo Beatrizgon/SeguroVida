8748eb168b2347c887638924c9c250b41fe55492
Beatriz dos Prazeres <beatrizgoncalves1251@gmail.com>
2024-11-30 17:37:44 -0300

    teste

---
+++ SeguroVida/redesSociais.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Care Up - Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> <!-- Faz o DROPDOWN aparecer as opções-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> <!-- Faz o DROPDOWN aparecer as opções-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- Referenciando a Navbar-->
+  <link rel="stylesheet" href="redes.css">
+  
+</head>
+
+                <!--NARBAR COMPLETA-->
+                <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">CareUp</a> 
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Planos</a> 
+      </li>
+      <li class="nav navbar-item">
+        <a class="nav-link" href="#">Planos Família</a>
+      </li> 
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="">Ajuda<span class="caret"></span></a>
+        <ul class="dropdown-menu"> <!--Terceira área com opções-->
+          <li><a href="#">Dúvidas</a></li>
+          <li><a href="#">Trabalhe Conosco</a></li>
+          <li><a href="redesSociais.php">Redes Sociais</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+                    <!--FIM NAVBAR-->
+
+<div class="container">
+  <h1>Fique à vontade para nos conhecer melhor nas redes sociais.</h1><br>
+  
+  <p><h3><a href="https://www.instagram.com/">Instagram</a></h3></p><br>
+  <p><h3><a href="https://www.facebook.com/login">Facebook</a></h3></p><br>
+  <p><h3><a href="https://web.whatsapp.com/">WhatsApp</a></h3></p>
+<br>
+<br>
+  <button>VOLTAR</button>
+</div>
+
+
+</body>
+</html>
